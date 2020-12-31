dcebc1dccd1cdb3ac67664cfdcbd457e9ffd9cc7
ahuacate15 <carlos.itca@gmail.com>
2020-12-30 20:33:44 -0600

    add bottom navigation menu

--- app/src/main/java/com/ahuacate/pigs/ui/MainActivity.kt
+++ app/src/main/java/com/ahuacate/pigs/ui/MainActivity.kt
@@ -1,13 +1,15 @@
-package com.ahuacate.pigs
+package com.ahuacate.pigs.ui
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.ahuacate.pigs.R
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
